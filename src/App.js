3d5aac15a2b9424a5c8b89cc68e2a4f111cdeef7
Jean Lobo <jeanlobocd@gmail.com>
2019-08-29 22:02:01 -0300

    chore: my fist commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import profile from "./assets/perfil.jpg";
+
+import TechList from "./components/TechList";
+
+function App() {
+  return <TechList />;
+  //return <img width="200" src={profile} />;
+  //return <h1>Hello Rocketseat!</h1>;
+}
+
+export default App;
