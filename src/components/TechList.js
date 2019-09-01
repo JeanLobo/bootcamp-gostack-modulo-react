90c4f46cbd15545b3a11915868d183ebf1107466
Jean Lobo <jeanlobocd@gmail.com>
2019-09-01 20:29:34 -0300

    chore: finalizado introdução ao React

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
-
+import TechItem from "./TechItem";
 class TechList extends Component {
   state = {
     newTech: "",
-    techs: ["Node.js", "ReactJS", "React Native"]
+    techs: []
+    //techs: ["Node.js", "ReactJS", "React Native"]
   };
+
+  // # Ínicio do Ciclo de Vida dos Componentes
+
+  // Executado assim que o componente aparece na tela
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado => componentDidUpdate(prevProps, prevState)
+  // Essa ideia pode ser utilizada para armazenar temporariamente os dados que o usuário preencheu em um formulário para não perder caso a internet ou o serviço do backend para de funcionar
+  componentDidUpdate(_, prevState) {
+    // this.props, this.state
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {}
+
+  // # Fim do Ciclo de Vida dos Componentes
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -30,12 +56,11 @@ class TechList extends Component {
       <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button">
-                Remover
-              </button>
-            </li>
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
           ))}
         </ul>
         <input
